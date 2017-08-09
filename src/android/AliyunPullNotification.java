06e17063cd5a9ce3f3cb1df47d9b1da9ecb39958
Steve <alucard263096@126.com>
2017-08-10 02:01:18 +0800

    嵌入android

--- src/android/AliyunPullNotification.java
+++ src/android/AliyunPullNotification.java
@@ -7,26 +7,46 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.util.Log;
+import com.alibaba.sdk.android.callback.InitResultCallback;
+import com.alibaba.sdk.android.push.CloudPushService;
+import com.alibaba.sdk.android.push.CommonCallback;
+import com.alibaba.sdk.android.push.noonesdk.PushServiceFactory;
+
 /**
  * This class echoes a string called from JavaScript.
  */
 public class AliyunPullNotification extends CordovaPlugin {
-
+	private static final String TAG = "AliyunPullNotification";
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("coolMethod")) {
-            String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
+        if (action.equals("startPull")) {
+            this.startPull("", callbackContext);
             return true;
         }
         return false;
     }
 
-    private void coolMethod(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            callbackContext.success(message);
-        } else {
+    private void startPull(String message, CallbackContext callbackContext) {
+		try{
+
+			PushServiceFactory.init(this.cordova.getActivity().getApplicationContext());
+			CloudPushService pushService = PushServiceFactory.getCloudPushService();
+			pushService.register(applicationContext, new CommonCallback() {
+				@Override
+				public void onSuccess(String response) {
+					Log.d(TAG, "init cloudchannel success");
+					callbackContext.success("success");
+				}
+				@Override
+				public void onFailed(String errorCode, String errorMessage) {
+					Log.d(TAG, "init cloudchannel failed -- errorcode:" + errorCode + " -- errorMessage:" + errorMessage);
+					callbackContext.error("Expected one non-empty string argument.");
+				}
+			});
+		}catch(){
             callbackContext.error("Expected one non-empty string argument.");
-        }
+		}
+		
     }
 }
